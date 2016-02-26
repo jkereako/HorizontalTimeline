c5c1ad0be8b543f9232f6281ab51cdf03183ef01
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-02-25 21:43:02 -0500

    Rewrite drawing code
    Created an IBDesignable to draw the horizontal timeline.

---
+++ Source/CircleView.swift
@@ -0,0 +1,58 @@
+//
+//  CircleView.swift
+//  HorizontalTimeline
+//
+//  Created by Jeff Kereakoglow on 2/25/16.
+//  Copyright © 2016 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+/// Creates a circle with a horizontal line running through the middle of it.
+@IBDesignable class CircleView: UIView {
+  // Circle
+  @IBInspectable var diameter: CGFloat = 50.0
+  @IBInspectable var strokeWidth: CGFloat = 2.0
+  @IBInspectable var strokeColor: UIColor = UIColor.clearColor()
+  @IBInspectable var fillColor: UIColor = UIColor.clearColor()
+
+  // Line
+  @IBInspectable var lineWidth: CGFloat = 2.0
+  @IBInspectable var lineColor: UIColor = UIColor.clearColor()
+
+  override func drawRect(rect: CGRect) {
+    layer.addSublayer(lineLayer())
+    layer.addSublayer(circleLayer())
+  }
+
+  private func circleLayer() -> CAShapeLayer {
+    let shapeLayer = CAShapeLayer()
+
+    shapeLayer.path = UIBezierPath(
+      ovalInRect: CGRect(x: 0, y: 0, width: diameter, height: diameter)).CGPath
+
+    shapeLayer.fillColor = fillColor.CGColor
+    shapeLayer.strokeColor = strokeColor.CGColor
+    shapeLayer.lineWidth = strokeWidth
+    shapeLayer.position = CGPoint(
+      x: bounds.width / 2.0 - (diameter / 2.0), y: bounds.height / 2.0 - (diameter / 2.0)
+    )
+
+    return shapeLayer
+  }
+
+  private func lineLayer() -> CAShapeLayer {
+    let shapeLayer = CAShapeLayer()
+    let path = UIBezierPath()
+    path.moveToPoint(CGPoint(x: 0, y: 0))
+    path.addLineToPoint(CGPoint(x: bounds.width, y: 0))
+    path.closePath()
+
+    shapeLayer.path = path.CGPath
+    shapeLayer.strokeColor = lineColor.CGColor
+    shapeLayer.lineWidth = lineWidth
+    shapeLayer.position = CGPoint(x: 0, y: bounds.height / 2.0)
+
+    return shapeLayer
+  }
+}
