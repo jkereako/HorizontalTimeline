22b6a11c92422ff4b0117b1698f5c03cad373f9d
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2017-08-14 15:41:27 -0400

    Update for Swift 3, Xcode 8

--- Source/HorizontalTimelineItem.swift
+++ Source/HorizontalTimelineItem.swift
@@ -9,51 +9,55 @@
 import UIKit
 
 /// Creates a circle with a horizontal line running through the middle of it.
-@IBDesignable class HorizontalTimelineItem: UIView {
-  // Circle
-  @IBInspectable var diameter: CGFloat = 50.0
-  @IBInspectable var strokeWidth: CGFloat = 2.0
-  @IBInspectable var strokeColor: UIColor = UIColor.clearColor()
-  @IBInspectable var fillColor: UIColor = UIColor.clearColor()
+@IBDesignable
+final class HorizontalTimelineItem: UIView {
+    // Circle
+    @IBInspectable var diameter: CGFloat = 50.0
+    @IBInspectable var strokeWidth: CGFloat = 2.0
+    @IBInspectable var strokeColor: UIColor = .clear
+    @IBInspectable var fillColor: UIColor = .clear
 
-  // Line
-  @IBInspectable var lineWidth: CGFloat = 2.0
-  @IBInspectable var lineColor: UIColor = UIColor.clearColor()
+    // Line
+    @IBInspectable var lineWidth: CGFloat = 2.0
+    @IBInspectable var lineColor: UIColor = .clear
 
-  override func drawRect(rect: CGRect) {
-    layer.addSublayer(lineLayer())
-    layer.addSublayer(circleLayer())
-  }
+    override func draw(_ rect: CGRect) {
+        layer.addSublayer(lineLayer())
+        layer.addSublayer(circleLayer())
+    }
+}
 
-  private func circleLayer() -> CAShapeLayer {
-    let shapeLayer = CAShapeLayer()
+// MARK: - Private helper methods
+private extension HorizontalTimelineItem {
+    func circleLayer() -> CAShapeLayer {
+        let shapeLayer = CAShapeLayer()
 
-    shapeLayer.path = UIBezierPath(
-      ovalInRect: CGRect(x: 0, y: 0, width: diameter, height: diameter)).CGPath
+        shapeLayer.path = UIBezierPath(
+            ovalIn: CGRect(x: 0, y: 0, width: diameter, height: diameter)).cgPath
 
-    shapeLayer.fillColor = fillColor.CGColor
-    shapeLayer.strokeColor = strokeColor.CGColor
-    shapeLayer.lineWidth = strokeWidth
-    shapeLayer.position = CGPoint(
-      x: CGRectGetMidX(bounds) - (diameter / 2.0),
-      y: CGRectGetMidY(bounds) - (diameter / 2.0)
-    )
+        shapeLayer.fillColor = fillColor.cgColor
+        shapeLayer.strokeColor = strokeColor.cgColor
+        shapeLayer.lineWidth = strokeWidth
+        shapeLayer.position = CGPoint(
+            x: bounds.midX - (diameter / 2.0),
+            y: bounds.midY - (diameter / 2.0)
+        )
 
-    return shapeLayer
-  }
+        return shapeLayer
+    }
 
-  private func lineLayer() -> CAShapeLayer {
-    let shapeLayer = CAShapeLayer()
-    let path = UIBezierPath()
-    path.moveToPoint(CGPoint(x: 0, y: 0))
-    path.addLineToPoint(CGPoint(x: bounds.width, y: 0))
-    path.closePath()
+    func lineLayer() -> CAShapeLayer {
+        let shapeLayer = CAShapeLayer()
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: bounds.width, y: 0))
+        path.close()
 
-    shapeLayer.path = path.CGPath
-    shapeLayer.strokeColor = lineColor.CGColor
-    shapeLayer.lineWidth = lineWidth
-    shapeLayer.position = CGPoint(x: 0, y: bounds.height / 2.0)
-
-    return shapeLayer
-  }
+        shapeLayer.path = path.cgPath
+        shapeLayer.strokeColor = lineColor.cgColor
+        shapeLayer.lineWidth = lineWidth
+        shapeLayer.position = CGPoint(x: 0, y: bounds.height / 2.0)
+        
+        return shapeLayer
+    }
 }
